921de92570b08c3205fd8df0b80b50aacf44a7a2
OodizzapandaoO <gh0st1990@mail.ru>
2018-04-03 02:41:25 +0700

    добавила структуру для параметров окна

--- src/game.h
+++ src/game.h
@@ -10,4 +10,15 @@ typedef struct{
 	int 		screen_number;
 	GC 			gc;
 }
+x_window_param_t;
 
+int 	window_init	(x_window_param_t *param, char *title, 
+					int pos_x, int pos_y, int Width, int Height);
+	
+void 	window_free	(x_window_param_t *param);
+
+typedef struct{	
+	char **content;	
+	unsigned capacity;
+	unsigned size;
+}
