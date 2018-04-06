963448e250fd1f49b8d856c61cdaaeaecd4a6ca4
OodizzapandaoO <gh0st1990@mail.ru>
2018-04-06 18:34:45 +0700

    завершение работы с game.h

--- src/game.h
+++ src/game.h
@@ -52,3 +52,32 @@ typedef enum{
 	GAME_PROGRESS,
 	GAME_OVER
 }
+game_status_t;
+
+typedef struct{
+	string_vec_t	*words_base;			
+	char			*current_word;
+	char			*word_progress;	
+	int				step_to_death;
+	game_status_t	status;
+}
+game_stat_t;
+
+int 	game_init				(game_stat_t *game, game_res_t *game_res);
+void 	game_letter_push		(game_stat_t *game, char *letter);	
+void 	game_letter_push_eng	(game_stat_t *game, char letter);	
+int 	game_reset				(game_stat_t *game);	
+void 	game_free				(game_stat_t *game);	
+char 	*game_return_progress	(game_stat_t *game);
+char 	*game_return_progress_eng	(game_stat_t *game);		
+int		game_win_check			(game_stat_t *game);
+int		game_lose_check			(game_stat_t *game);
+
+int return_letter_by_keycode(unsigned int keycode, char* output);
+int return_letter_by_keycode_eng(unsigned int keycode, char* output);
+
+void game_draw(x_window_param_t *win, game_res_t *res, game_stat_t *game);
+int pre_game_settings(x_window_param_t *win);
+void game_loop(x_window_param_t *win, game_res_t *res, game_stat_t *game);
+
+#endif
