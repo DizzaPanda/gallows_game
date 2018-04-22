6af6bd9830c45ec64046d5595700c5fcda479fc9
OodizzapandaoO <gh0st1990@mail.ru>
2018-04-22 21:31:01 +0700

    окно

--- src/game.c
+++ src/game.c
@@ -17,3 +17,31 @@ static void set_title(char *title_ptr, Display *display, Window window){
   		NULL, NULL, NULL, NULL, NULL,
   		NULL );
 }
+
+int window_init(x_window_param_t *param, char *title,				
+		int pos_x, int pos_y, int Width, int Height){
+			
+	param->display = XOpenDisplay(NULL);
+	if(param->display == NULL){
+		perror("Open display failed");
+        return 1;
+	}
+	
+	param->screen_number = DefaultScreen(param->display);
+	
+	param->window =  XCreateSimpleWindow(
+			param->display, RootWindow(param->display, param->screen_number), sudo
+            		BlackPixel(param->display, param->screen_number),
+			WhitePixel(param->display, param->screen_number));
+	
+	set_title(title, param->display, param->window);
+	
+	XSelectInput(param->display, param->window, KeyPressMask | KeyReleaseMask );
+	XMapWindow(param->display, param->window);
+	
+	param->gc = XCreateGC ( param->display, param->window, 0 , NULL );
+	
+	return 0;
+}
+
+
